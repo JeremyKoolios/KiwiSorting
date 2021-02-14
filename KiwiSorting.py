44f585423611ffb125215bdbc1f1003d1363fd05
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-02-15 10:05:53 +1200

    selection sort function implemented

--- KiwiSorting.py
+++ KiwiSorting.py
@@ -7,7 +7,7 @@ csvList = list(csvReader) #creates list out of csv
 file.close()
 
 
-
+'''
 #bubble sort function
 def bubbleSort(listName, sortIndex):
     n = len(listName)
@@ -16,22 +16,22 @@ def bubbleSort(listName, sortIndex):
             if(listName[j][sortIndex] > listName[j + 1][sortIndex]):
                 listName[j], listName[j + 1] = listName[j + 1], listName[j]
 
+
 bubbleSort(csvList, 2)
-
-
 '''
+
 #selection sort function
 def selectionSort(listName, sortIndex):
     n = len(listName)
+
     for i in range(n):
         minimum = i
         for j in range(i + 1, n):
             if(listName[minimum][sortIndex] > listName[j][sortIndex]):
                 minimum = j
-                listName[i], listName[minimum] = listName[minimum], listName[i]
+            listName[i], listName[minimum] = listName[minimum], listName[i]
 
 selectionSort(csvList, 2)
-'''
 
 #write into sorted csv
 file = open('C:/Users/jerem/Desktop/Dev/2021/Python/KiwiSorting/sortedKiwiData.csv', 'w') #open csv in write mode
